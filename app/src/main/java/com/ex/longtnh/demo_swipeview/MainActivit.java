9b8cce6ff326dfa6421bbfec8b7a4122641280c0
long.tnh <tnhoanglonghd@gmail.com>
2016-03-15 01:10:03 +0700

    thay đổi tí mà

--- app/src/main/java/com/ex/longtnh/demo_swipeview/MainActivit.java
+++ app/src/main/java/com/ex/longtnh/demo_swipeview/MainActivit.java
@@ -2,6 +2,7 @@ package com.ex.longtnh.demo_swipeview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivit extends AppCompatActivity {
 
@@ -11,5 +12,7 @@ public class MainActivit extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 //        tao đang test này
+        final TextView textView = (TextView)findViewById(R.id.tv);
+        textView.setText("Hài hước");
     }
 }
