16f498592972ad96ed8ce8afc882d6c8138a2d61
Truong Nguyen Hoang Long <long.tnh@Truongs-MacBook-Pro.local>
2016-03-15 01:01:42 +0700

    Initial commit

---
+++ app/src/main/java/com/ex/longtnh/demo_swipeview/MainActivit.java
@@ -0,0 +1,15 @@
+package com.ex.longtnh.demo_swipeview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
