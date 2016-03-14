56043aada0b9cf012daf9430d115fb08a18c2ef8
long.tnh <tnhoanglonghd@gmail.com>
2016-03-15 01:25:24 +0700

    đây nài

--- app/src/main/java/com/ex/longtnh/demo_swipeview/MainActivit.java
+++ app/src/main/java/com/ex/longtnh/demo_swipeview/MainActivit.java
@@ -10,7 +10,6 @@ public class MainActivit extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//      đậu xanh rau má nè con
+        //Cái mợ gì thế
     }
 }
